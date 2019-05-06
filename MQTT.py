0e86d4881cd7ffe29e468758017d5028c47f0031
elejcw <32955409+elejcw@users.noreply.github.com>
2019-05-07 00:03:39 +0100

    Update MQTT.py

--- MQTT.py
+++ MQTT.py
@@ -6,7 +6,7 @@ def timeStamped(fname, fmt='%Y-%m-%d-%H-%M_{fname}'):
     return (datetime.datetime.now()).strftime(fmt).format(fname=fname)
 
 
-broker_address="test.mosquitto.org."
+broker_address="broker.mqttdashboard.com"
 client = mqtt.Client("P1")                                
 client.connect("test.mosquitto.org.", 1883, 60)
 
